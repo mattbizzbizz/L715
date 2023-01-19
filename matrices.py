2c559ffeefcdbb6756728e68458a5cd7223d933b
mattbizzbizz <mattbizzbizz@gmail.com>
2023-01-19 11:53:53 -0500

    Finished assignment.

--- matrices.py
+++ matrices.py
@@ -26,18 +26,24 @@ def copy_matrix(M):
 def print_matrix(M):
     print('[', end = '')
     for i, row in enumerate(M):
-        if i == 0:
-            print('[', end = '')
+        if type(row) == list:
+            if i == 0:
+                print('[', end = '')
+            else:
+                print(' [', end = '')
+            for j, num in enumerate(row):
+                print(num, end = '')
+                if j != (len(row) - 1):
+                    print(', ', end = '')
+            if i != (len(M) - 1):
+                print('],')
+            else:
+                print(']', end = '')
         else:
-            print(' [', end = '')
-        for j, num in enumerate(row):
-            print(num, end = '')
-            if j != (len(row) - 1):
+            print(row, end = '')
+            if i != (len(M) - 1):
                 print(', ', end = '')
-        if i != (len(M) - 1):
-            print('],')
-        else:
-            print(']]')
+    print(']')
 
 def matrix_add(A, B):
     C = matrix_zeros(len(A), len(A[0]))
@@ -56,6 +62,22 @@ def matrix_multiply(A, B):
             C[i][k] = mult
     return C
 
+def matrix_transpose(A):
+    C = matrix_zeros(len(A[0]), len(A))
+    for i in range(len(A[0])):
+        for j in range(len(A)):
+            C[i][j] = A[j][i]
+    return C
+
+def matrix_flatten(A):
+    C = []
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            C.append(A[i][j])
+    return C
+
 B = [[6, 4], 
      [4, 8], 
      [3, 5]]
+print_matrix(B)
+print_matrix(matrix_flatten(B))
