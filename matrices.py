5c6a8b71c4397ef9229c8a213f1e75fb066450e4
mattbizzbizz <mattbizzbizz@gmail.com>
2023-01-19 15:51:43 -0500

    Finished assignment.

--- matrices.py
+++ matrices.py
@@ -1,56 +1,59 @@
-#def matrix_zeros(row, col):
-#    M = []
-#    for i in range(row):
-#        M.append([])
-#        for j in range(cols):
-#            M[i].append(0)
-#    return M
-
+# Create a matrix of zeros with dimensions rows x cols
 def matrix_zeros(rows, cols):
-    M = [
-            [0 for i in range(cols)]
-                    for j in range(rows)]
-    return M
+    return [[0] * cols for i in range(rows)]
 
-#def copy_matrix(M_in):
-#    M_out = matrix_zeros(len(M_in), len(M_in[0]))
-#    for i in range(len(M_in)):
-#        for j in range(len(M_in[0])):
-#            M_out[i][j] = M_in[i][j]
-#    return M_out
-
+# Create a copy of the matrix M
 def copy_matrix(M):
-    copy = [rows for rows in M]
-    return copy
+    return [rows[:] for rows in M]
 
-def print_matrix(M):
-    print('[', end = '')
-    for i, row in enumerate(M):
-        if type(row) == list:
-            if i == 0:
-                print('[', end = '')
-            else:
-                print(' [', end = '')
-            for j, num in enumerate(row):
-                print(num, end = '')
+# Print the matrix in such a format:
+#   [[6, 4], 
+#    [4, 8], 
+#    [3, 5]]
+def print_matrix(mat):
+
+    # Check if mat is a 1D matrix
+    if any(isinstance(elem, int) for elem in mat):
+        print(mat)
+
+    else:
+        print('[', end='')
+
+        # Iterate through rows of mat
+        for i, row in enumerate(mat):
+
+            # Add a space if this is NOT the first row
+            if i != 0:
+                print(' ', end='')
+
+            print('[', end='') # Add left bracket for start of row
+
+            # Iterate through elements of row
+            for j, col in enumerate(row):
+
+                print(col, end='') # print element
+
+                # Add a comma and space if this is NOT the last element of the row
+                #   else add right bracket
                 if j != (len(row) - 1):
-                    print(', ', end = '')
-            if i != (len(M) - 1):
-                print('],')
-            else:
-                print(']', end = '')
-        else:
-            print(row, end = '')
-            if i != (len(M) - 1):
-                print(', ', end = '')
-    print(']')
+                    print(', ', end='')
+                else:
+                    print(']', end = '')
+
+            if i != (len(mat) - 1):
+                print(',')
+
+        print(']')
 
 def matrix_add(A, B):
-    C = matrix_zeros(len(A), len(A[0]))
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            C[i][j] = A[i][j] + B[i][j]
-    return C
+    if (matrix_shape(A) != matrix_shape(B)):
+       raise ValueError('Cannot add arrays with different shapes.') 
+    C = []
+#    C = matrix_zeros(len(A), len(A[0]))
+#    for i in range(len(A)):
+#        for j in range(len(A[0])):
+#            C[i][j] = A[i][j] + B[i][j]
+    return [C.append(row + col)]
 
 def matrix_multiply(A, B):
     C = matrix_zeros(len(A), len(B[0]))
@@ -76,8 +79,30 @@ def matrix_flatten(A):
             C.append(A[i][j])
     return C
 
-B = [[6, 4], 
-     [4, 8], 
-     [3, 5]]
-print_matrix(B)
-print_matrix(matrix_flatten(B))
+def matrix_concat(A, B):
+
+    if len(A) != len(B):
+       raise ValueError('Cannot concatonate arrays with different number of rows.') 
+
+    C = matrix_zeros(len(A), len(A[0]) + len(B[0]))
+    for i, row in enumerate(A):
+        for j, num in enumerate(row):
+            C[i][j] = num
+    for i, row in enumerate(A):
+        for j, num in enumerate(row):
+            C[i][j + len(A[0])] = num
+    return C
+
+def matrix_shape(A):
+    shape = []
+    while isinstance(A, list):
+        shape.append(len(a))
+        A = A[0]
+    return shape
+
+def transpose_matrix(matrix):
+    transpose = []
+    for i in range(len(matrix[0])): # num col
+        transpose.append([matrix[j][i] for j in range(len(matrix))]) # num rows
+               
+    return transpose
