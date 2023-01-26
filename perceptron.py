a5ca5777e7a3b34b461e679b472bc7620e17f079
mattbizzbizz <mattbizzbizz@gmail.com>
2023-01-26 11:00:46 -0500

    Added plot.

--- perceptron.py
+++ perceptron.py
@@ -22,16 +22,27 @@ alpha = 0.001 # learning rate
 
 # Open .tsv file
 with open('perceptron_training_data.tsv') as file:
-    for index, line in enumerate(file):
+    next(file)
+    for line in file:
         l = line.strip('\n').split('\t')
-        if index:
-            l[0] = float(l[0])
-            l[1] = float(l[1])
-            l[2] = int(l[2])
-            print(l)
+        l[0] = float(l[0])
+        l[1] = float(l[1])
+        l[2] = int(l[2])
         df.append(l)
 
-arr = np.array(df)
+
+df_pos = []
+df_neg = []
+for line in df:
+    if line[2]:
+        df_pos.append(line)
+    else:
+        df_neg.append(line)
+
+minx = min([row[0] for row in df]) 
+maxx = max([row[0] for row in df]) 
+miny = min([row[1] for row in df])
+maxy = max([row[1] for row in df])
 
 weights.append(np.random.rand())
 weights.append(np.random.rand())
@@ -41,33 +52,37 @@ for i in range(epochs):
     print("Epoch:", i + 1)
     count = 0
 
-    for index, row in enumerate(arr):
+    for row in df:
 
-        if index:
+        x = [row[0], row[1]]
+        y_hat = 0.0
 
-            x = [row[0], row[1]]
-            y_hat = 0.0
+        if (np.dot(x, weights)) > 0:
+            y_hat = 1.0
 
-            if (np.dot(x, weights)) > 0:
-                y_hat = 1.0
-
-            if y_hat == line[2]:
-                count += 1
-            else:
-                if y_hat == 0:
-                    weights = np.add(weights, [alpha * value for value in x])
-                    bias += alpha
-                elif y_hat == 1:
-                    weights = np.subtract(weights, [alpha * value for value in x])
-                    bias -= alpha
-
-    print("Number of correctly classified examples:", count)
-    if count == len(arr) - 1:
-        print(weights)
-        break
+        if y_hat == row[2]:
+            count += 1
+        else:
+            if y_hat == 0:
+                weights = np.add(weights, [alpha * value for value in x])
+                bias += alpha
+            elif y_hat == 1:
+                weights = np.subtract(weights, [alpha * value for value in x])
+                bias -= alpha
 
     print(weights)
 
-fig, ax = plt.subplots()
-ax.scatter([i[0] for i in df], [i[1] for i in df])
-plt.show()
+    fig, ax = plt.subplots()
+    ax.set_xlim([minx - 0.5, maxx + 0.5])
+    ax.set_ylim([miny - 0.5, maxy + 0.5])
+    ax.set_title(f'Epoch {i}')
+    ax.scatter([j[0] for j in df_pos], [k[1] for k in df_pos], c = 'red')
+    ax.scatter([j[0] for j in df_neg], [k[1] for k in df_neg], c = 'blue')
+    y0 = (-bias - minx * weights[0]) / weights[1]
+    y1 = (-bias - maxx * weights[1]) / weights[1]
+    ax.plot([minx, maxx], [y0, y1])
+    plt.show()
+
+    print("Number of correctly classified examples:", count)
+    if count == len(df):
+        break
