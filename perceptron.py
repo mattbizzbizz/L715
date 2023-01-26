c809209cb0267db357d74d28b89db2643bddba5a
mattbizzbizz <mattbizzbizz@gmail.com>
2023-01-26 02:11:30 -0500

    Updated perceptron to add graph.

--- perceptron.py
+++ perceptron.py
@@ -10,53 +10,64 @@
 #  https://stackoverflow.com/questions/5825208/python-array-multiply
 
 import numpy as np
-import sys
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 weights = [] # list of weights
 df = [] # list perceptron training data
-epochs = int(sys.argv[1]) # number of epochs to run
+epochs = 25 # number of epochs to run
 bias = np.random.rand() # value for the bias
 alpha = 0.001 # learning rate
 
 # Open .tsv file
 with open('perceptron_training_data.tsv') as file:
-    next(file)
-    for line in file:
+    for index, line in enumerate(file):
         l = line.strip('\n').split('\t')
+        if index:
+            l[0] = float(l[0])
+            l[1] = float(l[1])
+            l[2] = int(l[2])
+            print(l)
         df.append(l)
 
-weights.append(np.random.rand())
-weights.append(np.random.rand())
+arr = np.array(df)
 
+weights.append(np.random.rand())
+weights.append(np.random.rand())
 
 for i in range(epochs):
 
     print("Epoch:", i + 1)
-
     count = 0
 
-    for line in df:
+    for index, row in enumerate(arr):
 
-        x = [float(line[0]), float(line[1])]
-        y_hat = 0.0
+        if index:
 
-        if (np.dot(x, weights)) > 0:
-            y_hat = 1.0
+            x = [row[0], row[1]]
+            y_hat = 0.0
 
-        if y_hat == float(line[2]):
-            count += 1
-        else:
+            if (np.dot(x, weights)) > 0:
+                y_hat = 1.0
 
-            if y_hat == 0:
-                weights = np.add(weights, [alpha * value for value in x])
-                bias += alpha
-            elif y_hat == 1:
-                weights = np.subtract(weights, [alpha * value for value in x])
-                bias -= alpha
+            if y_hat == line[2]:
+                count += 1
+            else:
+                if y_hat == 0:
+                    weights = np.add(weights, [alpha * value for value in x])
+                    bias += alpha
+                elif y_hat == 1:
+                    weights = np.subtract(weights, [alpha * value for value in x])
+                    bias -= alpha
 
     print("Number of correctly classified examples:", count)
-    if count == len(df):
+    if count == len(arr) - 1:
         print(weights)
         break
 
     print(weights)
+
+fig, ax = plt.subplots()
+ax.scatter([i[0] for i in df], [i[1] for i in df])
+plt.show()
