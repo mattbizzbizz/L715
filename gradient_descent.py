8763cb69d62d8324820f0ad2c82ad586c77cf3a8
mattbizzbizz <mattbizzbizz@gmail.com>
2023-02-07 23:15:40 -0500

    Finding issues with minibatched gradient descent.

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,44 +1,20 @@
 # References:
-#   https://numpy.org/doc/stable/reference/generated/
+#   https://numpy.org/
 
 import numpy as np
 from matplotlib import pyplot as plt
 
-#def gradient_descent(W, D, A, E, S, M):
-#
-#    C = []
-#    X = np.array([row[0] for row in D])
-#    Y = np.array([row[1] for row in D])
-#    Y_hat = []
-#
-#    for e in range(E):
-#
-#        if S:
-#            np.random.shuffle(D)
-#            X = np.array([row[0] for row in D])
-#            Y = np.array([row[1] for row in D])
-#
-#
-#        if S or M > 1:
-#            for i in range(0, len(Y), M):
-#                Y_hat = X[i:i+M] * W
-#                W = W - A * np.sum((X[i:i+M] * (Y_hat - Y[i:i+M]))) / len(X)
-#                C.append(0.5 / len(X) * np.sum(np.square(Y_hat - Y)))
-#            C = np.sum(C)
-#        else:
-#            Y_hat = X * W
-#            W = W - A * np.sum(np.multiply(X, Y_hat - Y)) / len(X)
-#            C.append(0.5 / len(X) * np.sum(np.square(Y_hat - Y)))
-#
-#    return C
-
+# Calculate costs for graident descent
 def gradient_descent(Weight, Data, Alpha, Epochs):
 
-    m = len(Data)
-    X = np.array([row[0] for row in Data])
-    Y = np.array([row[1] for row in Data])
-    cost = []
+    m = len(Data) # Number of training examples
+    X = np.array([row[0] for row in Data]) # Inputs
+    Y = np.array([row[1] for row in Data]) # Expected Outputs
+    cost = [] # Costs
 
+    # Iterate through epochs to:
+    #     (i) Calculate and store cost
+    #     (ii) Update weight
     for e in range(Epochs):
         diff = X * Weight - Y
         cost.append(np.dot(diff, diff) / (2 * m))
@@ -46,46 +22,55 @@ def gradient_descent(Weight, Data, Alpha, Epochs):
 
     return cost
 
+# Calculate costs for minibatched graident descent
 def minibatched_gradient_descent(Weight, Data, Alpha, Epochs):
 
-    m = len(Data)
-    X = np.array([row[0] for row in Data])
-    Y = np.array([row[1] for row in Data])
-    batch_size = 5
-    cost = []
+    m = len(Data) # Number of training examples
+    X = np.array([row[0] for row in Data]) # Inputs
+    Y = np.array([row[1] for row in Data]) # Expected Outputs
+    batch_size = 5 # Batch size
+    cost = [] # Costs
 
+    # Iterate through epochs to:
+    #     (i) Calculate and store cost
+    #     (ii) Update weight
     for e in range(Epochs):
 
         diff = X * Weight - Y
         cost.append(np.dot(diff, diff) / (2 * m))
 
+        # Iterate through data by batch size to:
+        #     (i) Update weight
         for batch in range(0, m, batch_size):
             diff = X[batch:batch + batch_size] * Weight - Y[batch:batch + batch_size]
             Weight -= Alpha * np.dot(X[batch:batch + batch_size], diff) / m
 
     return cost
 
-np.random.seed(3)
+np.random.seed(3) # Random seed
 
-epochs = 500
-alphas = [0.1, 0.01, 0.001]
+alphas = [0.1, 0.01, 0.001] # Learning rates
 
-x = np.random.randn(100, 1)
-y = 4 + 3 * x + np.random.randn(100, 1)
-data = np.concatenate((x, y), axis = 1)
-weight = np.random.rand()
-costs = {}
-costs_minibatch = {}
+x = np.random.randn(100, 1) # Inputs
+y = 4 + 3 * x + np.random.randn(100, 1) # Expected Outputs
+data = np.concatenate((x, y), axis = 1) # X & Y
 
+weight = np.random.rand() # Weight
+
+costs = {} # Costs for gradient descent
+costs_minibatch = {} # Cost for minibatched gradient descent
+
+# Add gradient descent to costs for each learning rate
 for alpha in alphas:
-    costs[alpha] = gradient_descent(weight, data, alpha, epochs)
+    costs[alpha] = gradient_descent(weight, data, alpha, 1000)
+
+# Add minibatched gradient descent to costs for each learning rate
 for alpha in alphas:
-    costs_minibatch[alpha] = minibatched_gradient_descent(weight, data, alpha, epochs)
+    costs_minibatch[alpha] = minibatched_gradient_descent(weight, data, alpha, 500)
 
-
+# Plot costs
 fi, ax = plt.subplots()
-for alpha in costs:
-    ax.plot(range(epochs), costs[alpha])
-    ax.plot(range(epochs), costs_minibatch[alpha])
-ax.legend(["\u03B1: 0.1", "\u03B1: 0.01", "\u03B1: 0.001", "\u03B1: 0.1 (Minibatch)", "\u03B1: 0.01 (Minibatch)", "\u03B1: 0.001 (Minibatch)"])
+for alpha in alphas:
+    ax.plot(range(1000), costs[alpha])
+    ax.plot(range(500), costs_minibatch[alpha])
 plt.show()
