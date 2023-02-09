b4d08942e02e5f21b9f1d5a8f7c53766b3eb3111
mattbizzbizz <mattbizzbizz@gmail.com>
2023-02-09 11:29:47 -0500

    Updated logistic regression.

--- logistic_regression.py
+++ logistic_regression.py
@@ -8,26 +8,38 @@ import numpy as np
 def sigmoid(Z):
     return 1 / (1 + np.exp(-Z))
 
+# Calculate y^
+#     (Z) y
+#     return y^
 def softmax(Z):
 
-    ret = []
+    ret = [] # y^
 
+    # Iterate through values in y
+    #     (i) Calculate y^
     for i, z in enumerate(Z):
         ret.append(np.exp(z) / np.sum([np.exp(z_i) for z_i in Z]))
 
     return np.array(ret)
 
-def cross_entropy_loss(X, Y, W, B):
-    S = sigmoid(np.dot(X, W) + B)
-    return -(Y * np.log(S) + (1 - Y) * np.log(1 - S))
+#def cross_entropy_loss(X, Y, W, B):
+#    S = sigmoid(np.dot(X, W) + B)
+#    return -(Y * np.log(S) + (1 - Y) * np.log(1 - S))
 
+# Do logistic regression for data
+#     (X) Data points
+#     (Y) Expected output
+#     (W) Weights
+#     (B) Bias
+#     (A) Learning rate
+#     (E) Number of epochs
+#     return array of losses across epochs
+def logistic_regression(X, Y, W, B, A, E):
 
-# Calculate costs for graident descent
-def stocastic_gradient_descent(X, Y, W, A, E):
-
-    m = len(X) # Number of training examples
-    cost = [] # Costs
-    indices = list(range(len(X)))
+    m = len(Y) # Number of data points
+    indices = list(range(m + 1)) # Indices of data points
+    costs = [] # Array of losses across epochs
+    XX = np.concatenate((X, np.ones((X.shape[0], 1))), axis = 1) # X with 1's in last row for partial derivative of b
 
     # Iterate through epochs to:
     #     (i) Calculate and store cost
@@ -35,22 +47,28 @@ def stocastic_gradient_descent(X, Y, W, A, E):
     for e in range(E):
 
         np.random.shuffle(indices) # Randomize indices
+        diff = softmax(np.array(np.dot(X, W) + B)) - Y # Calculate softmax(y^) - y
+        loss = np.empty([m + 1]) # Array of loss
 
+        # Iterate through randomized indices
+        #     (i) Calculate loss
+        #     (ii) Update weight
         for i in indices:
+            print('diff:', diff)
+            print('XX[i]', XX[i])
+            loss[i] = [diff * XX_i for XX_i in XX[i]]
+            W -= A * loss[i]
 
-            diff = X[i] * W - Y[i]
-            cost.append(np.dot(diff, diff) / (2 * m))
-            W -= A * np.dot(X[i], diff) / m
+        costs.append(loss) # Append loss matrix to costs
 
-    return cost
+    return costs
 
-def logistic_regression(X, Y, W, B, A, E):
-    return softmax(np.array(np.dot(X, W) + B))
+X, y = datasets.make_blobs(n_samples=100, n_features=3, centers=5, cluster_std=1.05, random_state=3) # Data points and expected output, respectively
 
-X, y = datasets.make_blobs(n_samples=100, n_features=3, centers=5, cluster_std=1.05, random_state=3)
+weights = np.array(np.random.randn(len(X[0]))) # Weights
+biases = np.array(np.random.randn(len(X))) # Biases
 
-weights = np.array(np.random.randn(len(X[0])))
-biases = np.array(np.random.randn(len(X)))
+logistic_regression(X, y, weights, biases, 0.1, 10)
 
 #fig = plt.figure()
 #ax = fig.add_subplot(projection='3d')
