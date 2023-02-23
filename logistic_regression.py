781a8b3ecd4c23bf5a883619fc1e7383b2807320
mattbizzbizz <mattbizzbizz@gmail.com>
2023-02-22 23:49:56 -0500

    Added graph.

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,81 +1,102 @@
-# Reference:
-#     https://numpy.org/
-
 from sklearn import datasets
 import matplotlib.pyplot as plt
 import numpy as np
 
-def sigmoid(Z):
-    return 1 / (1 + np.exp(-Z))
+X, Y = datasets.make_blobs(n_samples=100, n_features=3, centers=5, cluster_std=1.05, random_state=3) # Data points and expected output, respectively
 
-# Calculate y^
-#     (Z) y
-#     return y^
+np.random.seed(3)
+
+W = np.random.rand(3, 5)
+B = np.random.rand(5)
+
+alpha = 0.001
+epochs = 1000
+
+def one_hot_encoding(Y):
+    classes = list(set(Y))
+    onehot = np.zeros((len(Y), len(classes)))
+
+    for i, v in enumerate(Y):
+        onehot[i][v] = 1
+    return onehot
+
 def softmax(Z):
+    return np.exp(Z) / sum(np.exp(Z))
 
-    ret = [] # y^
+def predict(X, W, B):
+    return softmax((X @ W) + B)
 
-    # Iterate through values in y
-    #     (i) Calculate y^
-    for i, z in enumerate(Z):
-        ret.append(np.exp(z) / np.sum([np.exp(z_i) for z_i in Z]))
+def cost_function(y_hat, Y_ohe):
+    m = len(Y_ohe)
+    Y_log_Y_hat = np.log(y_hat) * Y_ohe
+    loss = -np.sum(Y_log_Y_hat) / m
+    return loss
 
-    return np.array(ret)
+def partial_derivative_w(X, diff):
+    return np.transpose(np.transpose(diff) @ X)
 
-#def cross_entropy_loss(X, Y, W, B):
-#    S = sigmoid(np.dot(X, W) + B)
-#    return -(Y * np.log(S) + (1 - Y) * np.log(1 - S))
+def partial_derivative_b(diff):
+    return np.sum(diff, axis = 0)
 
-# Do logistic regression for data
-#     (X) Data points
-#     (Y) Expected output
-#     (W) Weights
-#     (B) Bias
-#     (A) Learning rate
-#     (E) Number of epochs
-#     return array of losses across epochs
-def logistic_regression(X, Y, W, B, A, E):
+def update_parameters(W, B, A, X, Y_ohe):
 
-    m = len(Y) # Number of data points
-    print('m:', m)
-    indices = list(range(m)) # Indices of data points
-    print('indices:', indices)
-    costs = [] # Array of losses across epochs
-    XX = np.concatenate((X, np.ones((X.shape[0], 1))), axis = 1) # X with 1's in last row for partial derivative of b
-    print('length of XX:', len(XX))
+    m = len(X)
 
-    # Iterate through epochs to:
-    #     (i) Calculate and store cost
-    #     (ii) Update weight
-    for e in range(E):
+    y_hat = predict(X, W, B)
 
-        np.random.shuffle(indices) # Randomize indices
-        diff = softmax(np.array(np.dot(X, W) + B)) - Y # Calculate softmax(y^) - y
-        loss = np.empty([m + 1]) # Array of loss
+    cost = cost_function(y_hat, Y_ohe)
+    diff = y_hat - Y_ohe
 
-        # Iterate through randomized indices
-        #     (i) Calculate loss
-        #     (ii) Update weight
-        for i in indices:
-            print('i:', i)
-            loss[i] = np.sum([diff * XX_i for XX_i in XX[i]])
-            W -= A * loss[i]
+    partial_der_w = partial_derivative_w(X, diff)
+    partial_der_b = partial_derivative_b(diff)
 
-        costs.append(loss) # Append loss matrix to costs
+    W -= alpha * partial_der_w / m
+    B -= alpha * partial_der_b / m
 
-    return costs
+    return W, B, cost, y_hat
 
-X, y = datasets.make_blobs(n_samples=100, n_features=3, centers=5, cluster_std=1.05, random_state=3) # Data points and expected output, respectively
+def accuracy_metric(Y, y_hat):
+    m = len(Y)
+    correct = 0
 
-weights = np.array(np.random.randn(len(X[0]))) # Weights
-biases = np.array(np.random.randn(len(X))) # Biases
+    acc_classes = {i:[0, 0] for i in range(5)}
 
-#logistic_regression(X, y, weights, biases, 0.1, 10)
+    for y_true, y_pred in zip(Y, y_hat):
 
-#fig = plt.figure()
-#ax = fig.add_subplot(projection='3d')
-#ax.scatter(X[:,0], X[:,1], X[:,2], c=y)
-#plt.show()
-#
-#for x, y in list(zip(X, y))[:10]:
-#    print(x, y)
+        y_pred = np.argmax(y_pred)
+
+        acc_classes[y_true][0] += 1
+        if y_pred == y_true:
+            acc_classes[y_true][1] += 1
+            correct += 1
+
+    return acc_classes, correct * 100 / m
+
+cost_per_epoch = []
+acc_list = []
+acc_classes = []
+Y_ohe = one_hot_encoding(Y)
+
+for epoch in range(epochs):
+
+    print(f'------This is epoch {epoch + 1}------')
+    print(f'-- Old weights: {W}')
+    print(f'-- Old bias: {B}')
+
+    W, B, cost, y_pred = update_parameters(W, B, alpha, X, Y_ohe)
+    acc_classes, acc = accuracy_metric(Y, y_pred)
+    acc_list.append(acc_classes)
+    cost_per_epoch.append(cost)
+
+    print(f'-- New weights: {W}')
+    print(f'-- New bias: {B}')
+    print(f'-- Accuracy: {acc}')
+    print('\n')
+
+fig = plt.figure()
+ax = fig.add_subplot()
+print(acc_list[:10])
+for class_ in acc_list[0]:
+    ax.plot(range(epochs), [epoch[class_][1] for epoch in acc_list])
+ax.legend(["Class " + str(i + 1) for i in range(5)])
+plt.show()
